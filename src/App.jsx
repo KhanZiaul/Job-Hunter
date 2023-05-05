b245963ac8048e6c13a66603e44210106c694b20
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-06 01:11:34 +0600

    App Provider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Nav from './Components/Nav/Nav';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h2 className='text-5xl'>ok</h2>
+    <div className='mx-10'>
+      <div className='bg-slate-50 '>
+      <Nav></Nav>
+      </div>
+      <div className='md:min-h-[calc(100vh-48px)]'>
+        <Outlet></Outlet>
+      </div>
     </div>
   );
 };
